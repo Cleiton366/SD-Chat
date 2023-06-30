2ca0ec72183d7caa6b1ba694ba7e42d5fc7aa642
Cleiton <cleiton.queiroz@proton.me>
2023-06-30 17:59:00 -0300

    feat: SSL/TLS connection encryption

--- Client.py
+++ Client.py
@@ -1,5 +1,13 @@
 import socket
 import threading
+import ssl
+import datetime
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 HEADER = 64
 PORT = 3000
@@ -9,8 +17,50 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS = (SERVER, PORT)
 USER_NAME = ""
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDRESS)
+def generate_self_signed_cert():
+    # Generate a new private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    # Create a self-signed certificate
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"BR"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Ceará"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"Quixadá"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"UFC"),
+        x509.NameAttribute(NameOID.COMMON_NAME, u"localhost"),
+    ])
+    cert = x509.CertificateBuilder().subject_name(
+    subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        private_key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        datetime.datetime.utcnow() + datetime.timedelta(days=365)
+    ).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+        critical=False,
+    ).sign(private_key, hashes.SHA256(), default_backend())
+
+
+    # Convert private key and certificate to PEM format
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
+
+    return private_key_pem, cert_pem
+
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -24,6 +74,15 @@ def handle_messages():
         print(data)
 
 def start():
+    #GENERATING self-sign certificates for encrypted connection
+    private_key, cert = generate_self_signed_cert()
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    context.load_cert_chain(certfile=cert, keyfile=private_key)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client = context.wrap_socket(client, ADDRESS)
+    client.connect(ADDRESS)
+
     thread = threading.Thread(target=handle_messages)
     thread.start()
 
